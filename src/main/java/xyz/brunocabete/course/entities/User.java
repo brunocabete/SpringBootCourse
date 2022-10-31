790dc3e1e86de34bd726e0d89ffe7ec130786d65
Bruno Pandini Cabete <bruno.pandini@sigmagis.local>
2022-10-31 16:56:07 -0300

    H2 Database, test profile, JPA

--- src/main/java/xyz/brunocabete/course/entities/User.java
+++ src/main/java/xyz/brunocabete/course/entities/User.java
@@ -1,9 +1,20 @@
 package xyz.brunocabete.course.entities;
 
 import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -14,7 +25,7 @@ public class User implements Serializable {
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
-		super();
+	
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -87,6 +98,4 @@ public class User implements Serializable {
 		return true;
 	}
 
-	
-	
 }
